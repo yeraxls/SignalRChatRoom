770250c69d9f5c703872f4510988b05b776aaf85
yeraxls <yerayls91@gmail.com>
2021-06-08 21:46:21 +0200

    Agregar archivos de proyecto.

---
+++ ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoom
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string room, string user, string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task AddToGroup(string room)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+            await Clients.Group(room).SendAsync("ShowWho", 
+                $"Alguien se conectó {Context.ConnectionId}");
+        }
+
+    }
+}
---
+++ Controllers/ChatController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoom.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> Rooms = new Dictionary<int, string>
+        {
+            {1, "Cervezas" },
+            {2, "Programación" },
+            {3, "Moda" }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
